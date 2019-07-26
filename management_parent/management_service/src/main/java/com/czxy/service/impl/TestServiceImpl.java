0dd02c058954d556f1246b1d85faa9a9edaae6fd
任鑫磊 <1259497991@qq.com>
2019-07-26 15:13:53 +0800

    第二次提交代码，增加伪类

---
+++ management_parent/management_service/src/main/java/com/czxy/service/impl/TestServiceImpl.java
@@ -0,0 +1,14 @@
+package com.czxy.service.impl;
+
+import com.czxy.service.TestService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author: Mr.Ren
+ * @version: v 1.0
+ * @date: 2019/7/26
+ * @info:
+ */
+@Service
+public class TestServiceImpl implements TestService {
+}
