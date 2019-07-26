5a3d7b42d387a0c9683b3327a597df4e4d71e02d
任鑫磊 <1259497991@qq.com>
2019-07-26 11:16:38 +0800

    第一次提交项目 本次提交为项目包结构，项目中无任何功能代码

---
+++ management_parent/management_web/src/main/java/com/czxy/AppStarter.java
@@ -0,0 +1,17 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Mr.Ren
+ * @version: v 1.0
+ * @date: 2019/7/26
+ * @info:
+ */
+@SpringBootApplication
+public class AppStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStarter.class, args);
+    }
+}
